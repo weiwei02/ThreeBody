fe6c60cbe596e0f1e0310f56d24d58a6f1efdfbb
weiwei <weiwei02@vip.qq.com>
2018-05-09 00:49:31 +0800

    枚举

---
+++ kotlinLearn/src/main/java/enum/Shap.kt
@@ -0,0 +1,10 @@
+package enum
+
+/** cotlin 枚举
+ * @author Wang Weiwei <email>weiwei02@vip.qq.com / wangweiwei12@jd.com</email>
+ * @sine 2018/5/9
+ * @version 1.0
+ */
+enum class Shap(val chineseName: String){
+    SQUARE("正方形")
+}
---
+++ kotlinLearn/src/main/java/enum/package-info.java
@@ -0,0 +1,6 @@
+/** 枚举类包
+ * @author Wang Weiwei <email>weiwei02@vip.qq.com / wangweiwei12@jd.com</email>
+ * @sine 2018/5/8
+ * @version 1.0
+ */
+package data;
