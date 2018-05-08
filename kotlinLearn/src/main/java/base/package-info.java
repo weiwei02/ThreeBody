08b010c4064df972cd4762254dd226bd00a0dea0
weiwei <weiwei02@vip.qq.com>
2018-05-08 23:45:32 +0800

    数据类与密封类

---
+++ kotlinLearn/src/main/java/base/package-info.java
@@ -0,0 +1,6 @@
+/** cotlin基础语法
+ * @author Wang Weiwei <email>weiwei02@vip.qq.com / wangweiwei12@jd.com</email>
+ * @sine 2018/5/8
+ * @version 1.0
+ */
+package base;
---
+++ kotlinLearn/src/main/java/data/package-info.java
@@ -0,0 +1,6 @@
+/** 数据类与密封类
+ * @author Wang Weiwei <email>weiwei02@vip.qq.com / wangweiwei12@jd.com</email>
+ * @sine 2018/5/8
+ * @version 1.0
+ */
+package data;
