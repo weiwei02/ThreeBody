da5b522bdb6741f4a8ebb1334388cbff79d58342
weiwei <weiwei02@vip.qq.com>
2018-05-10 00:34:19 +0800

    增加spring kotlin 测试

---
+++ kotlin_spring/src/main/kotlin/com/github/weiwei02/threebody/kotlin_spring/config/BatchCofig.kt
@@ -0,0 +1,7 @@
+package com.github.weiwei02.threebody.kotlin_spring.config
+
+/**
+ * @author Wang Weiwei <email>weiwei02@vip.qq.com / wangweiwei12@jd.com</email>
+ * @sine 2018/5/10
+ * @version 1.0
+ */
